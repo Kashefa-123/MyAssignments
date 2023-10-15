bf17abb71cf8ac5b08054acdc04a7448f8ae3b16
Kashefa-123 <147966599+Kashefa-123@users.noreply.github.com>
2023-10-14 23:07:00 -0700

    Add files via upload
    day1

---
+++ day1/FindNumber.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class FindNumber {
+	
+	public static void main(String[] args) {
+		
+		int MyNumber = 45;
+		if (MyNumber>0) {
+			System.out.println("The number is positive");
+			
+		} else {
+			System.out.println("The number is negative");
+
+		}
+	}
+
+}
